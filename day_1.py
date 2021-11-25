1a6cd6f6207e96a87cf1d85bc1905e2ed21bef9b
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-11-25 19:04:46 +0200

    Day 1 Project: Band Name Generator

--- day_1.py
+++ day_1.py
@@ -1,12 +1,17 @@
 """
-Description : Part of 100 
+Description : Part of 100 Days of Code - The Complete
+Python Pro Bootcamp for 2022
+
+Day 1 : Working with variables in Python to Manage
+Data
+
+Day 1 Project: Band Name Generator
 
 
 Author: Ahmed Harbi
 Date: November 2021
 
 """
-
 
 city_name = input("What is the name of the city you grow up in?\n")
 pet_name = input("What is the name of your pet?\n")
