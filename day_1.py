83e5146a3cef0a847e7399976905a05c509d505a
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-11-25 14:42:36 +0200

    initialize

---
+++ day_1.py
@@ -0,0 +1,6 @@
+city_name = input("What is the name of the city you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+band_name = city_name + " " + pet_name
+
+print("your band name could be " + '"' + band_name + '"')
