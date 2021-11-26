4b51f6e73f74b44ede420f20ab3981232b603155
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-11-26 18:34:35 +0200

    Day 6

---
+++ Day 6/day_6.py
@@ -0,0 +1,33 @@
+"""
+Description : Part of 100 Days of Code - The Complete
+Python Pro Bootcamp for 2022
+
+Day 6 : Python Functions and Karel
+
+Day 6 Project: Escaping the Maze
+
+
+Author: Ahmed Harbi
+Date: November 2021
+
+"""
+
+"https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json"
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+    
+# while(not at_goal()):
+#     if (wall_in_front()):
+#         if right_is_clear():
+#             turn_right()
+#         else:
+#             turn_left()
+#     else:
+#         move()
+
