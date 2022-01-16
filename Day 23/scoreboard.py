fc1b183c0d0e377945a95b71e52284874d3f184f
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2022-01-16 10:32:31 +0200

    Day 23

---
+++ Day 23/scoreboard.py
@@ -0,0 +1,26 @@
+FONT = ("Courier", 24, "normal")
+
+from turtle import Turtle
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.goto(-280, 260)
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.clear()
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
